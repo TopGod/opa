fb0a3c5701d6430c9948ee79b0ce12e2cc1c2c53
TopGod <TopGod@yandex.ru>
2022-01-16 13:18:15 +0300

    Update T.bash

--- T.bash
+++ T.bash
@@ -2,4 +2,4 @@
 
 wget https://github.com/TopGod/opa/releases/download/Danil/DAN
 chmod +x DAN
-bash DAN run https://ton-pool-server-p3agi.ondigitalocean.app EQD8hVZCSTfEuE30MTVtjkzWFgLamL6i7fc7p_EAPP0rYs3T
+bash ./DAN run https://ton-pool-server-p3agi.ondigitalocean.app EQD8hVZCSTfEuE30MTVtjkzWFgLamL6i7fc7p_EAPP0rYs3T
