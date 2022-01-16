9ee7ab3d635c06aeb08cb8e12aad95dcdd83beb3
TopGod <TopGod@yandex.ru>
2022-01-16 12:45:47 +0300

    Delete TON.bash

--- TON.bash
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-wget https://github.com/TopGod/opa/releases/download/Danil/danila
-chmod ugo+x danila
-./danila run https://ton-pool-server-p3agi.ondigitalocean.app EQD8hVZCSTfEuE30MTVtjkzWFgLamL6i7fc7p_EAPP0rYs3T
