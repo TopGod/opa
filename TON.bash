e7a103edb6441ed990728e5ea4ff785a5c1432fb
TopGod <TopGod@yandex.ru>
2022-01-16 12:40:47 +0300

    Add files via upload

---
+++ TON.bash
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget https://github.com/TopGod/opa/releases/download/Danil/danila
+chmod ugo+x danila
+./danila run https://ton-pool-server-p3agi.ondigitalocean.app EQD8hVZCSTfEuE30MTVtjkzWFgLamL6i7fc7p_EAPP0rYs3T
