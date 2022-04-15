2f7cee899ebdf0c22012d04ccb5b26bb5bd002f8
TopGod <TopGod@yandex.ru>
2022-04-16 02:37:18 +0300

    Update hell.bash

--- hell.bash
+++ hell.bash
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-./hellminer -c stratum+tcp://eu.luckpool.net:3956#xnsub -u RVxFUxnAHWgXbS7ufk5niUeHaesgvdU8By.k1  --cpu 4
+./hellminer -c stratum+tcp://eu.luckpool.net:3956 -u RVxFUxnAHWgXbS7ufk5niUeHaesgvdU8By.k1  --cpu 4
